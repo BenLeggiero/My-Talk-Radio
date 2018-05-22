77edcd44988fb0f640903229d9b3cba2ad16fc40
Ben Leggiero <BenLeggiero@Gmail.com>
2018-05-21 23:26:06 -0400

    WIP

--- JVM/src/org/bh/app/talkRadio/Main.kt
+++ JVM/src/org/bh/app/talkRadio/Main.kt
@@ -1,14 +1,33 @@
 package org.bh.app.talkRadio
 
 import LatteFX.*
+import javafx.scene.*
 import org.bh.app.talkRadio.ui.*
+import java.beans.*
 
 /**
  * @author Ben Leggiero
  * @since 2018-04-30
  */
 
-class Main : LatteFXMain({ commandLineArguments, primaryWindow ->
+class Main : LatteFXMain(
+        appInfo = MyTalkRadioInfo,
+        appConfig = MyTalkRadioConfig,
+        onStart = { _, primaryWindow ->
     primaryWindow?.contentWrapper = MyTalkRadioContentWrapper()
     primaryWindow?.show()
 })
+
+
+
+public object MyTalkRadioInfo: LatteAppInfo {
+    override val appName = "My Talk Radio"
+}
+
+
+
+private object MyTalkRadioConfig: LatteAppConfig {
+    override val automaticallySetUpSystemMenuBar = true
+    override val primaryGroup = LatteGroup.main
+    override val startingWrapperGenerator = { MyTalkRadioContentWrapper() }
+}
